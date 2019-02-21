f8276f7a81fee989b69908b19f68fc8fd5781abd
Luisa3010 <luisa.kalkert@gmx.de>
2019-02-21 22:04:27 +0100

    Added some Stuff

--- Vr Test4/Assets/EnemyMovement.cs
+++ Vr Test4/Assets/EnemyMovement.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
 
     public GameObject Player;
     public float speed = 0.01f;
+    NavMeshAgent agent;
     
-    
+    void start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 PlayerPos = new Vector3(Player.transform.position.x,0.5f, Player.transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, PlayerPos, Time.deltaTime);
-        transform.LookAt(PlayerPos);
+        //Vector3 PlayerPos = new Vector3(Player.transform.position.x,0.5f, Player.transform.position.z);
+        //transform.position = Vector3.Lerp(transform.position, PlayerPos, Time.deltaTime);
+        //transform.LookAt(PlayerPos);
+        
+        GetComponent<NavMeshAgent>().destination = Player.transform.position;
     }
 }
