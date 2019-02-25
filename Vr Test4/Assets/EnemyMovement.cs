2564635bd48766cb67c6db00d0e6eafb160c9e6e
Luisa3010 <luisa.kalkert@gmx.de>
2019-02-25 12:06:46 +0100

    Changed Terrain

--- Vr Test4/Assets/EnemyMovement.cs
+++ Vr Test4/Assets/EnemyMovement.cs
@@ -7,21 +7,19 @@ public class EnemyMovement : MonoBehaviour
 {
 
     public GameObject Player;
-    public float speed = 0.01f;
     NavMeshAgent agent;
     
     void start()
     {
         agent = GetComponent<NavMeshAgent>();
+       
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        //Vector3 PlayerPos = new Vector3(Player.transform.position.x,0.5f, Player.transform.position.z);
-        //transform.position = Vector3.Lerp(transform.position, PlayerPos, Time.deltaTime);
-        //transform.LookAt(PlayerPos);
-        
+
         GetComponent<NavMeshAgent>().destination = Player.transform.position;
+       
     }
 }
